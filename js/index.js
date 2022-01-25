3c6d85868724b04ee720e5104eb4035cfb6943c6
YushengChou <mm7217373@gmail.com>
2022-01-25 19:35:37 +0800

    新增CSS JS資料夾

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// var cm = document.getElementById('Circle');
+
+// window.addEventListener('mousemove', function(e){
+//     var a = e.pageX
+//     var b = e.pageY
+//     // 增加滑鼠移動事件，回傳滑鼠定位座標
+//     function move(){
+//         console.log(a)
+//         cm.style.top = (b+50) + "px";
+//         cm.style.left = (a+50) + "px";
+//     }
+//     move();
+// })
