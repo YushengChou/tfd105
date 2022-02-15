e4322380daf7894f8c05334d3cb60d72381ef840
YushengChou <mm7217373@gmail.com>
2022-02-15 22:59:36 +0800

    add rwd for carousel

--- js/index.js
+++ js/index.js
@@ -5,18 +5,18 @@ let slideList = document.getElementById('CaroList');
 let prevSlide = document.getElementById('PrevSlide');
 let nextSlide = document.getElementById('NextSlide');
 let dotBtn = document.getElementsByClassName('dot_btn');
+let dotItem = document.getElementsByClassName('dot_item');
 let num;
 let index = 0;
-// 初始化
-dotBtn[index].classList.add('js-btnbgc');
+let itemWidth = document.getElementsByClassName('carousel_item')[0].offsetWidth;
 
 function slide(d) {
     let newLeft = parseInt(slideList.style.left) + d;
     slideList.style.left = newLeft + 'px';
     if (newLeft > 0) {
-        slideList.style.left = -3600 + 'px';
+        slideList.style.left = -(itemWidth*3) + 'px';
     }
-    if (newLeft === -4800) {
+    if (newLeft === -(itemWidth*4)) {
         slideList.style.left = 0 + 'px';
     }
 }
@@ -27,7 +27,7 @@ prevSlide.addEventListener('click', function () {
         index = 3;
     }
     btnShow();
-    slide(1200);
+    slide(itemWidth);
 })
 // 右側按鈕
 nextSlide.addEventListener('click', function () {
@@ -36,21 +36,18 @@ nextSlide.addEventListener('click', function () {
         index = 0;
     }
     btnShow();
-    slide(-1200);
+    slide(-(itemWidth));
 })
 // 每隔三秒執行
 function play() {
     num = setInterval(function () {
         nextSlide.click();
-    }, 3000)
+    }, 30000)
 }
-play();
 // 滑鼠滑入停止，移開繼續
 function stop() {
     clearInterval(num);
 }
-slideList.onmouseover = stop;
-slideList.onmouseout = play;
 // dot輪到的加上顏色
 function btnShow() {
     for (let i = 0; i < dotBtn.length; i++) {
@@ -59,10 +56,16 @@ function btnShow() {
     dotBtn[index].classList.add('js-btnbgc');
 }
 // dot click切換至該圖
-for (let i = 0; i < dotBtn.length; i++) {
-    dotBtn[i].addEventListener('click', function () {
+for (let i = 0; i < dotItem.length; i++) {
+    dotItem[i].addEventListener('click', function () {
         index = i;
         btnShow();
-        slideList.style.left = i * -1200 + 'px';
+        slideList.style.left = i * -(itemWidth) + 'px';
     })
 }
+// 初始化
+slideList.style.left = 0 + 'px';
+dotBtn[index].classList.add('js-btnbgc');
+play();
+slideList.onmouseover = stop;
+slideList.onmouseout = play;
