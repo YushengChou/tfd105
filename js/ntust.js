e0b84cb73c6861ae4e2d514aa30c2abbdad77987
DESKTOP-RTCT2GL\user <mm7217373@gmail.com>
2022-11-17 16:42:44 +0800

    add ntust

---
+++ js/ntust.js
@@ -0,0 +1,34 @@
+"use strict";
+
+$('#ok').on('click', function() {
+    let td1 = $('#boxDate').val()
+    let td2 = $('#boxBook').val()
+    let td3 = $('#boxOpen').val()
+    let td4 = $('#boxSheet').val()
+    let td5 = $('#boxDetail').val()
+    let td6 = $('#boxBasic').val()
+    let td7 = $('#boxMoney').val()
+    let td8 = $('#boxAmount').val()
+    let td9 = $('#boxRound').val()
+    let td10 = $('#boxAmounts').val()
+    let td11 = $('#boxPack').val()
+    let td12 = $('#boxCount').val()
+    let td13 = $('#boxCounts').val()
+    $('#dataBody').append(`
+        <tr>
+            <td>${td1}</td>
+            <td>${td2}</td>
+            <td>${td3}</td>
+            <td>${td4}</td>
+            <td>${td5}</td>
+            <td>${td6}</td>
+            <td>${td7}</td>
+            <td>${td8}</td>
+            <td>${td9}</td>
+            <td>${td10}</td>
+            <td>${td11}</td>
+            <td>${td12}</td>
+            <td>${td13}</td>
+        </tr>
+    `)
+})
