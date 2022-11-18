eba8eb4d3231c00f10f00e8a76567f66b89810f5
DESKTOP-RTCT2GL\user <mm7217373@gmail.com>
2022-11-18 12:28:56 +0800

    add login for ntust

--- js/ntust.js
+++ js/ntust.js
@@ -1,5 +1,22 @@
 "use strict";
 
+// 登入
+$('#signIn').on('click', function() {
+    if ($('#pwd').val() != '0000') {
+        swal({
+            title: "密碼錯誤",
+            type: "error"
+        })
+    } else {
+        swal({
+            title: "登入成功",
+            type: "success"
+        }).then(function() {
+            $('.mask').remove()
+        })
+    }
+})
+
 $('#boxAmount, #boxRound, #boxMoney, #boxBasic').on('blur', function() {
     // 包數 = 回數
     $('#boxPack').val($('#boxAmount').val())
