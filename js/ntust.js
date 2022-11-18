3538af0f4ff626f3c5927ebd2616c1f56d7e58b9
DESKTOP-RTCT2GL\user <mm7217373@gmail.com>
2022-11-18 11:15:34 +0800

    modify ntust ui

--- js/ntust.js
+++ js/ntust.js
@@ -1,9 +1,20 @@
 "use strict";
 
 $('#boxAmount, #boxRound, #boxMoney').on('blur', function() {
+    // 包數 = 回數
     $('#boxPack').val($('#boxAmount').val())
+    // 本數 = 本 * 回
     $('#boxAmounts').val($('#boxAmount').val() * $('#boxRound').val())
-    $('#boxCount').val($('#boxAmounts').val() * $('#boxMoney').val())
+    // 小計 用基本價判斷
+    if ($('#boxBasic').val() * $('#boxAmount').val() * $('#boxRound').val() < 1200) {
+        $('#boxCount').val(1200)
+    } else {
+        $('#boxCount').val($('#boxBasic').val() * $('#boxAmount').val() * $('#boxRound').val())
+    }
+    // 包裝價錢 = 單價 * 包數
+    $('#boxCount2').val($('#boxMoney').val() * $('#boxPack').val())
+    // 合計
+    $('#boxCounts').val(parseInt($('#boxCount').val()) + parseInt($('#boxCount2').val()))
 })
 
 $('#ok').on('click', function() {
@@ -16,15 +27,16 @@ $('#ok').on('click', function() {
         let td1 = $('#boxDate').val()
         let td2 = $('#boxBook').val()
         let td3 = $('#boxOpen').val()
-        let td4 = $('#boxSheet').val()
-        let td5 = $('#boxDetail').val()
-        let td6 = $('#boxBasic').val()
-        let td7 = $('#boxMoney').val()
-        let td8 = $('#boxAmount').val()
-        let td9 = $('#boxRound').val()
-        let td10 = $('#boxAmounts').val()
+        let td4 = $('#boxDetail').val()
+        let td5 = $('#boxBasic').val()
+        let td6 = $('#boxAmount').val()
+        let td7 = $('#boxRound').val()
+        let td8 = $('#boxAmounts').val()
+        let td9 = $('#boxCount').val()
+
+        let td10 = $('#boxMoney').val()
         let td11 = $('#boxPack').val()
-        let td12 = $('#boxCount').val()
+        let td12 = $('#boxCount2').val()
         let td13 = $('#boxCounts').val()
         $('#dataBody').append(`
             <tr>
@@ -32,14 +44,23 @@ $('#ok').on('click', function() {
                 <td>${td2}</td>
                 <td>${td3}</td>
                 <td>${td4}</td>
-                <td>${td5}</td>
-                <td>基本價${td6}元</td>
-                <td>${td7}元</td>
+                <td>基本價${td5}元</td>
+                <td>${td6}本</td>
+                <td>${td7}回</td>
                 <td>${td8}本</td>
-                <td>${td9}回</td>
-                <td>${td10}本</td>
-                <td>${td11}包</td>
+                <td>${td9}元</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="3"></td>
+                <td>包裝</td>
+                <td>${td10}元</td>
+                <td colspan="3">${td11}包</td>
                 <td>${td12}元</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="9"></td>
                 <td>${td13}元</td>
             </tr>
         `)
@@ -60,5 +81,6 @@ $('#ok').on('click', function() {
         $('#boxPack').val('')
         $('#boxCount').val('')
         $('#boxCounts').val('')
+        $('#boxCount2').val('')
     }
 })
